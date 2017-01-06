3012b1163c79fb2e21a544d3ff878713db74059b
Federico Naranjo <federico.naranjo@gmail.com>
2017-01-06 11:32:01 -0500

    test setup/cleanup and collections

--- xUnit Notes.cs
+++ xUnit Notes.cs
@@ -117,6 +117,8 @@ Setting Up xUnit
     // Resharper >> Extension Manager
     // add xUnit.net Test Support...
 
+using Xunit;
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
 Test Types
@@ -194,26 +196,135 @@ Assert.IsAsssignableFrom<Type>(obj);
 –––––––––––––––––––––––––––––––
 Categories
 –––––––––––––––––––––––––––––––
+/* Organizing tests by categories */
+// takes input of name, value pairs
+[Trait("Category","Error Checking")]
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
-<><><><><><><><><><><><><><>
+Test Output
 –––––––––––––––––––––––––––––––
+/* Test Helper Interface */
+using Xunit.Abstractions;
+
+// create output helper in test class
+private readonly ITestOutputHelper output;
+
+// create constructor with helper, so xUnit can use it
+// xUnit handles output automatically
+public TestClass(ITestOutputHelper helper) {
+    output = helper;
+}
+
+// use just like Console.WriteLine
+output.WriteLine("message");
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
-<><><><><><><><><><><><><><>
+Setup and Clean up
 –––––––––––––––––––––––––––––––
+/* Setup (individual tests) */
+
+// in Test Class constructor
+// this will be called before EACH test
+public TestClass(ITestOutputHelper helper) {
+    output = helper;
+    sut = new Sut();    // system under test
+}
+
+/* Clean up (individual tests) */
+
+// Test Class must be child of IDisposible 
+public class TestClass : IDisposible {
+    // clean up method
+    // called after EACH test
+    public void Dispose() {
+        sut.Delete();   // or other clean up methods
+    }
+}
+
+/* Setup (test suite) */
+
+// must make Test Class child of IClassFixture<T>
+// must create custom class T
+public class CustomFixture {
+    public Sut sut { get; private set; }
+    public CustomFixture() {
+        sut = new Sut();
+    }
+}
+
+public class TestClass : IClassFixture<CustomFixture> {
+    private readonly CustomFixture fixture;
+    private readonly ITestOutputHelper output;
+    // constructor still called before each test,
+        // can be used to reset previous test
+    public TestClass(ITestOutputHelper helper, CustomFixture fix) {...}
+}
+
+/* Clean up (test suite) */
+
+// must make fixture child of IDisposible
+public class CustomFixture : IDisposible {
+    public Sut sut { get; private set; }
+    public CustomFixture() {
+        sut = new Sut();
+    }
+    // clean up method to run after all tests
+    public void Dispose() {
+        sut.Delete();
+    }
+}
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
-<><><><><><><><><><><><><><>
+Collection Fixtures
 –––––––––––––––––––––––––––––––
+/* Creating a Fixture to be used by multiple Test Suites */
+
+// use attributes to assign Test Suite to collection
+[Collection("My Collection")]
+public class TestClass{}
+
+// create collection definition
+[CollectionDefinition("My Collection")]
+public class MyCollection : ICollectionFixture<CustomFixture> {}
+
+// fixture will be created only once
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
-<><><><><><><><><><><><><><>
+Parallel Execution
 –––––––––––––––––––––––––––––––
+// All tests within different collections will run in parallel
+
+/* All tests in assembly considered same collection (no parallel) */
+    // in AssemblyInfo.cs, change to:
+    [assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly)]
+
+/* All tests to run in parallel (unless same collection) */
+    // in AssemblyInfo.cs, comment out:
+    [assembly: CollectionBehavior(CollectionBehavior.CollectionPerClass)]
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
-<><><><><><><><><><><><><><>
+Collection Execution Order
 –––––––––––––––––––––––––––––––
+/* Test Collection Orderer */
+
+// need to create class to order tests
+public class CollectionOrderer : ITestCollectionOrderer {
+    public IEnumberable<ITestCollection>OrderCollections(IEnumberable<ITestCollection> testCollections) {
+        // sort/order tests here
+        return testCollections;
+    }
+}
+
+/* Configure */
+
+// AssemblyInfo.cs:
+[assembly: TestCollectionOrderer("AssemblyName.CollectionOrderer","AssemblyName.tests")]
+
 /*---------------------------*/
 –––––––––––––––––––––––––––––––
 <><><><><><><><><><><><><><>
